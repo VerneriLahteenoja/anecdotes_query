36cb69ea27c08d4f96668d45b2ee98e4c8ab88a3
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-04-26 15:53:23 +0300

    create add anecdote functionality

--- src/services/requests.js
+++ src/services/requests.js
@@ -6,3 +6,7 @@ const baseUrl = 'http://localhost:3001/anecdotes'
 export const getAll = () => {
   return axios.get(baseUrl).then(res => res.data)
 }
+
+export const createOne = (newAnecdote) => {
+  return axios.post(baseUrl, newAnecdote).then(res => res.data)
+}
