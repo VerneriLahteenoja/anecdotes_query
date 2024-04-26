7157c14f1e30b366f49e31ffd85e5e159157e5c9
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2024-04-26 15:30:29 +0300

    fetch data from json server with react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAll } from './services/requests'
+import { useQuery } from '@tanstack/react-query'
+
 
 const App = () => {
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAll,
+    retry: 1
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  } else if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
---
+++ src/services/requests.js
@@ -0,0 +1,8 @@
+import axios from "axios"
+
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+export const getAll = () => {
+  return axios.get(baseUrl).then(res => res.data)
+}
